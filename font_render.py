1415b143bcd20f38aada6e838fc3a20a510d028b
TaylorAMort <taylor.tomlinson1396@gmail.com>
2026-04-03 15:57:45 -0600

    Fixing font again

--- font_render.py
+++ font_render.py
@@ -1,7 +1,9 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def opening_screen():
-    font = pygame.font.Font("elvencommonspeak-2.ttf", 120)
+    font = pygame.font.Font("fonts/elvencommonspeak-2.ttf", 120)
     text_surface = font.render("Seven Shadows of the Shattered Blade", True, (255, 255, 255))
     return screen.blit(text_surface, (SCREEN_WIDTH // 2 - text_surface.get_width() // 2, SCREEN_HEIGHT // 2 - text_surface.get_height() // 2))
